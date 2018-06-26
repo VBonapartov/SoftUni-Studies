af591637f7a9738766ec716593d204b348845dc3
VBonapartov <32267603+VBonapartov@users.noreply.github.com>
2018-06-26 14:45:57 +0300

    Update 01. Number of Users for Email Provider.sql

--- DB Basics - MSSQL Server/08. Additional Exercises/SQL Scripts/01. Number of Users for Email Provider.sql
+++ DB Basics - MSSQL Server/08. Additional Exercises/SQL Scripts/01. Number of Users for Email Provider.sql
@@ -3,7 +3,7 @@ USE [Diablo]
 GO
 
   SELECT RIGHT(Email, LEN(Email) - CHARINDEX('@', Email)) AS [Email Provider], 
-		     COUNT(*) AS [Number of Users]
+	 COUNT(*) AS [Number of Users]
     FROM Users
 GROUP BY RIGHT(Email, LEN(Email) - CHARINDEX('@', Email))
 ORDER BY [Number of Users] DESC, [Email Provider]
