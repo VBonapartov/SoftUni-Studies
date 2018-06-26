541675591761cc82e8c69382bb482460c214951f
VBonapartov <32267603+VBonapartov@users.noreply.github.com>
2018-06-26 14:47:16 +0300

    Update 06. Display All Items with Information about Forbidden Game Type.sql

--- DB Basics - MSSQL Server/08. Additional Exercises/SQL Scripts/06. Display All Items with Information about Forbidden Game Type.sql
+++ DB Basics - MSSQL Server/08. Additional Exercises/SQL Scripts/06. Display All Items with Information about Forbidden Game Type.sql
@@ -2,13 +2,13 @@
 USE [Diablo]
 GO
 
-		     SELECT i.Name AS [Item], 
-				        i.Price,
-				        i.MinLevel,
-				        gt.Name AS [Forbidden Game Type]
-		       FROM Items AS i
+	 SELECT i.Name AS [Item], 
+		i.Price,
+		i.MinLevel,
+		gt.Name AS [Forbidden Game Type]
+	   FROM Items AS i
 LEFT OUTER JOIN GameTypeForbiddenItems AS fi ON fi.ItemId = i.Id
 LEFT OUTER JOIN GameTypes AS gt ON gt.Id = fi.GameTypeId
-	     ORDER BY [Forbidden Game Type] DESC, [Item] ASC
+       ORDER BY [Forbidden Game Type] DESC, [Item] ASC
 
 
